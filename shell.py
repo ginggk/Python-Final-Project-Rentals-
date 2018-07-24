b46bf30561661cfaf46407a652f7ab51c638b3d3
gingerkeys <gkeys@basecampcodingacademy.org>
2018-07-24 11:26:36 -0500

    implemented welcome into shell and started to begin string formatting

---
+++ shell.py
@@ -0,0 +1,17 @@
+# import core
+# import disk
+
+
+def welcome():
+    print("Welcome to GB's Book Rental Store!!")
+
+
+
+def main():
+    welcome()
+    print("{0} 'Uglies' by Scott Westerfield is 10 dollars a day. \n {1} 'Unwind' by Neal Shusterman is 16 dollars a day. \n {2} 'Binge' by Tyler Oakley is 20 dollars a day.").format(.., .., ..)
+    response = input("What book would you like to rent today?")
+
+
+if __name__ == '__main__':
+    main()
