02df719bd0b9ce3dfb1a26cae623f47c0a5fc6fd
gingerkeys <gkeys@basecampcodingacademy.org>
2018-07-25 13:56:59 -0500

    added a function to shell and printed it

--- shell.py
+++ shell.py
@@ -6,40 +6,7 @@ def welcome():
     print("Welcome to GB's Book Rental Store!!")
 
 
-def user():
-    print(
-        "'Uglies' by Scott Westerfield is 10 dollars a day. \n 'Unwind' by Neal Shusterman is 16 dollars a day. \n 'Binge' by Tyler Oakley is 20 dollars a day."
-    )
-    response = input("What book would you like to rent today?").strip()
-    if response == 'Uglies':
-        print("It will be {} for 5 days.".format(
-            disk.inventory['Book1']['Price']))
-    elif response == 'Unwind':
-        print("It will be {} for 5 days.".format(
-            disk.inventory['Book2']['Price']))
-    elif response == 'Binge':
-        print("It will be {} for 5 days.".format(
-            disk.inventory['Book3']['Price']))
-
-
-def employee():
-    while True:
-        employee = input(
-            "Would you like to see what is in [s]tock, [t]ransaction history, or [c]alculate the total revenue?"
-        ).strip().lower()
-        if employee == 's' or employee == 'in stock':
-            print("stock")
-        elif employee == 'h' or employee == 'history':
-            print('history')
-        elif employee == 't' or employee == 'total revenue':
-            print('Total Revenue')
-        else:
-            print(
-                "That is not a valid response, please put in a valid choice!")
-
-
-def main():
-    welcome()
+def user_employee():
     while True:
         comment = input(
             "Are you a [1]employee or a [2]customer?").strip().lower()
@@ -51,7 +18,65 @@ def main():
             print(
                 "Invalid choice! Please tell us if you are a customer or a trusted employee!"
             )
+
+
+def make_inventory():
+    inventory = open_inventory()
+    return inventory
+
+
+def user():
+    # inventory = open_inventory()
+    while True:
+        print(
+            "'Uglies' by Scott Westerfield is 10 dollars a day. \n 'Unwind' by Neal Shusterman is 16 dollars a day. \n 'Binge' by Tyler Oakley is 20 dollars a day."
+        )
+        print("------------------------------------------------")
+        response = input(
+            "What book would you like to rent today?\n (type [d]one when you are finished shopping)\n"
+        ).strip()
+        if response == 'Uglies':
+            print("We have {} in stock today of Uglies.").format()
+            print("It will be {}  dollars for 5 days.".format(
+                core.set_days(10, 5)))
+            print("With Sales Tax your total is: ").format(...)
+        elif response == 'Unwind':
+            print("It will be {} dollars for 5 days.".format(
+                core.set_days(16, 5)))
+            print("With Sales Tax your total is: ").format(...)
+        elif response == 'Binge':
+            print("It will be {} dollars for 5 days.".format(
+                core.set_days(20, 5)))
+            print("With Sales Tax your total is: ").format(...)
+        elif response == 'd' or response == 'done':
             break
+        else:
+            print(
+                "We are sorry, we do not have that book. Please choose a provided book."
+            )
+
+
+def employee():
+    while True:
+        choice = input(
+            "Would you like to see what is in [s]tock, [t]ransaction history, or [c]alculate the total revenue?"
+        ).strip().lower()
+        if choice == 's' or choice == 'in stock':
+            print("stock")
+        elif choice == 'h' or choice == 'history':
+            print('history')
+        elif choice == 't' or choice == 'total revenue':
+            print('Total Revenue')
+        else:
+            print(
+                "That is not a valid response, please put in a valid choice!")
+
+
+def main():
+    welcome()
+    user_employee()
+    make_inventory()
+    inventory_information = open_inventory()
 
 
 if __name__ == '__main__':
