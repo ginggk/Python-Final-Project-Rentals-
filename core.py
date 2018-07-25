df33e9bce84fc35fe62f7e8cd64a187a893ff50c
gingerkeys <gkeys@basecampcodingacademy.org>
2018-07-25 13:57:32 -0500

    implemented one funciton and working on the second.

--- core.py
+++ core.py
@@ -1,22 +1,11 @@
-def create_inventory(inv_list):
-    inventory_dictionary = {}
-    for inventory in inv_list:
-        items = inventor.split(',')
-        key = items[0].strip()
-        value = int(items[1].strip())
-        inventory_dictionary[key] = value
-    return inventory_dictionary
-
-
-def make_inventory_string(inventory_dictionary):
-    '''{str, int, int, int, number, int} -> str
-    Returns the user_dictionary into a string.
+def set_days(price, days):
+    '''float -> float
+    Returns the price for a given number of days, given the price.
     '''
-    inventory_string = 'name, stock, price, replacement fee, sales tax, deposit'
-    for name, stock, price, replacement_fee, sales_tax, deposit in inventory_dictionary.items(
-    ):
-        inventory_string += '\n{},{}, {}, {}, {}, {}'.format(
-            name, stock, price, replacement_fee, sales_tax, deposit)
-    return inventory_dictionaryrent():
-    
+    return days * price
 
+
+def sales_tax(total):
+    """number -> float
+    Returns the total with the sales tax added to it.
+    """
