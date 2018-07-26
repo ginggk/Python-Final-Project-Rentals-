028a6b3e1b84b07df72a16b1b776a19a4166f791
gingerkeys <gkeys@basecampcodingacademy.org>
2018-07-26 16:35:37 -0500

    added my stock function and writing function into shell

--- shell.py
+++ shell.py
@@ -29,52 +29,67 @@ def user():
     inventory_information = disk.open_inventory()
     inventory_dictionary = disk.create_inventory(inventory_information)
     while True:
-        print(
-            " 'Uglies' by Scott Westerfield is 10 dollars a day. \n 'Unwind' by Neal Shusterman is 16 dollars a day. \n 'Binge' by Tyler Oakley is 20 dollars a day."
-        )
-        print("------------------------------------------------")
-        response = input(
-            " What book would you like to rent today?\n (type [d]one when you are finished shopping)\n"
-        ).strip()
-        if response == 'Uglies':
-            print("{} has {} copies in stock.".format(
-                inventory_dictionary['Uglies']['Name'],
-                inventory_dictionary['Uglies']['In Stock']))
-            print("It will be {}  dollars for 5 days.".format(set_days(10, 5)))
-            print("With Sales Tax your total is: ${}".format(
-                sales_tax(set_days(10, 5))))
-            print("----------------------------------------------")
-        elif response == 'Unwind':
-            print("{} has {} copies in stock.".format(
-                inventory_dictionary['Unwind']['Name'],
-                inventory_dictionary['Unwind']['In Stock']))
-            print("It will be {} dollars for 5 days.".format(set_days(16, 5)))
-            print("With Sales Tax your total is: ${}".format(
-                round(sales_tax(set_days(16, 5)))))
-            print("----------------------------------------------")
-        elif response == 'Binge':
-            print("{} has {} copies in stock.".format(
-                inventory_dictionary['Binge']['Name'],
-                inventory_dictionary['Binge']['In Stock']))
-            print("It will be {} dollars for 5 days.".format(set_days(20, 5)))
-            print("With Sales Tax your total is: ${}".format(
-                round(sales_tax(set_days(20, 5)))))
-            print("----------------------------------------------")
-        elif response == 'd' or response == 'done':
-            break
-        else:
+        while True:
+            keep = input("Would you like to return or rent a book today?")
+            if keep == 'return':
+                print(bring_back)
+            else:
+                break
+        while True:
             print(
-                "We are sorry, we do not have that book. Please choose a provided book."
+                " 'Uglies' by Scott Westerfield is 10 dollars a day. \n 'Unwind' by Neal Shusterman is 16 dollars a day. \n 'Binge' by Tyler Oakley is 20 dollars a day."
             )
+            print("------------------------------------------------")
+            response = input(
+                " What book would you like to rent today?\n (type [d]one when you are finished shopping)\n"
+            ).strip()
+            if response == 'Uglies':
+                print("{} has {} copies in stock.".format(
+                    inventory_dictionary['Uglies']['Name'],
+                    inventory_dictionary['Uglies']['In Stock']))
+                print("It will be {}  dollars for 5 days.".format(
+                    set_days(10, 5)))
+                print("With Sales Tax your total is: ${}".format(
+                    sales_tax(set_days(10, 5))))
+                update_stock(inventory_dictionary, 'Uglies')
+                print("----------------------------------------------")
+            elif response == 'Unwind':
+                print("{} has {} copies in stock.".format(
+                    inventory_dictionary['Unwind']['Name'],
+                    inventory_dictionary['Unwind']['In Stock']))
+                print("It will be {} dollars for 5 days.".format(
+                    set_days(16, 5)))
+                print("With Sales Tax your total is: ${}".format(
+                    round(sales_tax(set_days(16, 5)))))
+                update_stock(inventory_dictionary, 'Unwind')
+                print("----------------------------------------------")
+            elif response == 'Binge':
+                print("{} has {} copies in stock.".format(
+                    inventory_dictionary['Binge']['Name'],
+                    inventory_dictionary['Binge']['In Stock']))
+                print("It will be {} dollars for 5 days.".format(
+                    set_days(20, 5)))
+                print("With Sales Tax your total is: ${}".format(
+                    round(sales_tax(set_days(20, 5)))))
+                update_stock(inventory_dictionary, 'Binge')
+                print("----------------------------------------------")
+            elif response == 'd' or response == 'done':
+                break
+            else:
+                print(
+                    "We are sorry, we do not have that book. Please choose a provided book."
+                )
 
 
 def employee():
+    inventory_information = disk.open_inventory()
+    inventory_dictionary = disk.create_inventory(inventory_information)
     while True:
         choice = input(
             "Would you like to see what is in [s]tock, [t]ransaction history, or [c]alculate the total revenue?"
         ).strip().lower()
         if choice == 's' or choice == 'in stock':
-            print("stock")
+            print(make_inventory_string(inventory_dictionary))
         elif choice == 'h' or choice == 'history':
             print('history')
         elif choice == 't' or choice == 'total revenue':
