eec94179ae5556b891353a8d2d6f2c179718f1c4
gingerkeys <gkeys@basecampcodingacademy.org>
2018-07-26 09:27:57 -0500

    both functions in core are tested now.

--- test_core.py
+++ test_core.py
@@ -4,3 +4,9 @@ from core import *
 def test_set_days():
     assert set_days(10, 5) == 50
     assert set_days(0, 0) == 0
+
+
+def test_sales_tax():
+    assert sales_tax(50) == 53.5
+    assert sales_tax(68.9) == 73.72300000000001
+    assert sales_tax(set_days(10, 5)) == 53.5
