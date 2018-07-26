a2fc0c1e35bb6619842984af38921d4f69beeeeb
gingerkeys <gkeys@basecampcodingacademy.org>
2018-07-26 11:37:19 -0500

    tested 3 out of 4 functions.

--- test_core.py
+++ test_core.py
@@ -1,5 +1,7 @@
 from core import *
 
+# from bcca.test import fake_file, should_print
+
 
 def test_set_days():
     assert set_days(10, 5) == 50
@@ -10,3 +12,59 @@ def test_sales_tax():
     assert sales_tax(50) == 53.5
     assert sales_tax(68.9) == 73.72300000000001
     assert sales_tax(set_days(10, 5)) == 53.5
+
+
+# def test_check_stock():
+#     inventory_dictionary = {
+#         'table': {
+#             'name': 'table',
+#             'stock': 12,
+#             'price': 40,
+#         },
+#         'chair': {
+#             'name': 'chair',
+#             'stock': 12,
+#             'price': 40,
+#         }
+#     }
+
+#     item = 'chair'
+
+#     # stock = inventory_dictionary['chair']['stock']
+
+#     check_stock(inventory_dictionary, item)
+
+#     assert {
+#         'table': {
+#             'name': 'table',
+#             'stock': 12,
+#             'price': 40,
+#         },
+#         'chair': {
+#             'name': 'chair',
+#             'stock': 11,
+#             'price': 40,
+#         }
+#     } == inventory_dictionary
+
+
+def test_make_inventory_string():
+
+    inventory_dictionary = {
+        'table': {
+            'name': 'table',
+            'stock': 12,
+            'price': 40,
+            'color': 'black'
+        },
+        'chair': {
+            'name': 'chair',
+            'stock': 12,
+            'price': 40,
+            'color': 'brown'
+        }
+    }
+
+    make_inventory_string(inventory_dictionary)
+
+    assert 'Chair, 12, 40'
