7c050de94171ec443921091890f8df359b4e61f6
gingerkeys <gkeys@basecampcodingacademy.org>
2018-08-15 15:37:32 -0500

    sean went in and brooke the functions into smaller functions with ginger. in main, the rent and return functions are currently returning None. ginger will need to have those functions return a transaction string to write to the history

--- shell.py
+++ shell.py
@@ -10,27 +10,19 @@ def user_employee():
     while True:
         comment = input(
             "Are you a [1]employee or a [2]customer?(if you would like to quit, please type (q).\n"
-        ).strip().lower()
-        if comment == 'customer' or comment == '2':
-            user()
-        elif comment == 'employee' or comment == '1':
-            employee()
+        ).lower().strip()
+        if comment in ['customer', '2']:
+            return 'customer'
+        elif comment in ['employee', '1']:
+            return 'employee'
         elif comment == 'q' or comment == 'quit':
-            break
-        else:
-            print(
-                "Invalid choice! Please tell us if you are a customer or a trusted employee!"
-            )
+            return 'quit'
+        print(
+            "Invalid choice! Please tell us if you are a customer or a trusted employee!"
+        )
 
 
-# def make_inventory():
-# inventory = open_inventory()
-# return inventory
-
-
-def bring_back():
-    inventory_information = disk.open_inventory()
-    inventory_dictionary = disk.create_inventory(inventory_information)
+def bring_back(inventory_dictionary):
     while True:
         print('-------------------------------------------------')
         core.printable_inventory(inventory_dictionary)
@@ -39,9 +31,6 @@ def bring_back():
             "Which of these books would you like to return today?").strip()
         if back in inventory_dictionary:
             core.book_return(inventory_dictionary, back)
-            disk.write_to_inventory(inventory_dictionary)
-            disk.write_to_history(
-                core.negative_dep(inventory_dictionary, back))
             print("Here is your deposit...")
             print("Thank you for reading one of our books!")
             break
@@ -49,51 +38,48 @@ def bring_back():
             print("That is not one of our book, try again please.")
 
 
-def user():
-    inventory_information = disk.open_inventory()
-    inventory_dictionary = disk.create_inventory(inventory_information)
+def get_rent_or_return():
     while True:
-        while True:
-            print('--------------------------------------------------')
-            keep = input(
-                "Would you like to return or rent a book today?\n(if you would like to go back, type(b)\n"
-            ).strip().lower()
-            if keep == 'return':
-                bring_back()
-            elif keep == 'b' or keep == 'back':
-                return
-            else:
-                break
-        while True:
-            print('-------------------------------------------------')
-            core.printable_inventory(inventory_dictionary)
-            print("------------------------------------------------")
-            response = input(
-                " What book would you like to rent today?\n (type [d]one when you are finished shopping)\n"
-            ).strip()
-            if response in inventory_dictionary:
-                if core.check_stock(inventory_dictionary, response) == True:
-                    print(
-                        "Sorry! That book is out of stock, please choose another one of our quality books!"
-                    )
-                else:
-                    print("-----------------------------------")
-                    print(
-                        core.make_book_sentence(inventory_dictionary,
-                                                response))
-                    core.update_stock(inventory_dictionary, response)
-                    disk.write_to_inventory(inventory_dictionary)
-                    disk.write_to_history(
-                        inventory_dictionary[response]['Replacement Fee'])
-            else:
+        print('--------------------------------------------------')
+        keep = input(
+            "Would you like to return or rent a book today?\n(if you would like to go back, type(b)\n"
+        ).strip().lower()
+        if keep in ['rent', 'return']:
+            return keep
+        elif keep in ['back', 'b']:
+            return 'back'
+        print('invalid!!')
+
+
+def user_select_book(inventory_dictionary):
+    while True:
+        print('-------------------------------------------------')
+        core.printable_inventory(inventory_dictionary)
+        print("------------------------------------------------")
+        response = input(
+            " What book would you like to rent today?\n (type [d]one when you are finished shopping)\n"
+        ).strip()
+        if response in inventory_dictionary.keys():
+            if core.check_stock(inventory_dictionary, response) == True:
                 print(
-                    "We are sorry, we do not have that book. Please choose a provided book."
+                    "Sorry! That book is out of stock, please choose another one of our quality books!"
                 )
+            else:
+                return response
+        else:
+            print(
+                "We are sorry, we do not have that book. Please choose a provided book."
+            )
+
+
+def user_rent(inventory_dictionary):
+    response = user_select_book(inventory_dictionary)
+    print('you rented', core.make_book_sentence(inventory_dictionary,
+                                                response))
+    core.update_stock(inventory_dictionary, response)
 
 
 def employee():
-    inventory_information = disk.open_inventory()
-    inventory_dictionary = disk.create_inventory(inventory_information)
     while True:
         choice = input(
             "Would you like to see what is in [s]tock, transaction [h]istory, or calculate the [t]otal revenue?\n (type [d]one when finished.) \n"
@@ -115,10 +101,24 @@ def main():
     welcome()
     inventory_information = disk.open_inventory()
     inventory_dictionary = disk.create_inventory(inventory_information)
-    user_employee()
-    # make_inventory()
-    # inventory_information = open_inventory()
-    # inventory_dictionary = disk.create_inventory(inventory_information)
+    while True:
+        decision = user_employee()
+        if decision == 'customer':
+            rent_or_return = get_rent_or_return()
+            if rent_or_return in ['rent', 'return']:
+                if rent_or_return == 'rent':
+                    transaction = user_rent(inventory_dictionary)
+                elif rent_or_return == 'return':
+                    transaction = bring_back(inventory_dictionary)
+                disk.write_to_inventory(inventory_dictionary)
+                disk.write_to_history(transaction)
+            else:
+                continue
+        elif decision == 'employee':
+            employee(inventory_dictionary)
+        elif decision == 'quit':
+            print('exiting program...')
+            exit()
 
 
 if __name__ == '__main__':
